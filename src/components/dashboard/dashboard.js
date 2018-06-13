d80ad60299bb7eb6ff0cbcc1db59a46d7d5a90b8
Diksha Sach <sachdiksha@gmail.com>
2018-06-13 00:11:40 -0400

    init commit

---
+++ src/components/dashboard/dashboard.js
@@ -0,0 +1,186 @@
+import React from 'react';
+import { clearAuth } from "../actions/auth";
+import { connect } from "react-redux";
+import { clearAuthToken } from "../../local-storage";
+import requiresLogin from "../../requires-login";
+import { withRouter } from "react-router-dom";
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Home from '@material-ui/icons/Home';
+import Settings from '@material-ui/icons/Settings';
+import SettingsPower from '@material-ui/icons/SettingsPower';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+
+import './dashboard.css';
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    height: 430,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing.unit * 7,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing.unit * 9,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+export class Dashboard extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
+  logOut() {
+    this.props.dispatch(clearAuth());
+    clearAuthToken();
+  }
+
+  render() {
+    const { classes, theme } = this.props;
+   
+    return (
+      <div className={classes.root}>
+        <AppBar
+          position="absolute"
+          className={classNames(classes.appBar, this.state.open && classes.appBarShift)}
+        >
+          <Toolbar disableGutters={!this.state.open}>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={this.handleDrawerOpen}
+              className={classNames(classes.menuButton, this.state.open && classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" noWrap>
+              Dashboard
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
+          }}
+          open={this.state.open}
+        >
+          <div className={classes.toolbar}>
+            <IconButton onClick={this.handleDrawerClose}>
+              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            <div className="list-menu-icon"><Home style={{fontSize: '58px'}}/> Home</div>
+            <Divider/>
+            <div className="list-menu-icon"><Settings style={{fontSize: '58px'}}/> Settings</div>
+            <Divider/>
+            <div className="list-menu-icon"><SettingsPower style={{fontSize: '58px'}} onClick={() => this.logOut()}/> LogOut</div>
+            <Divider/>
+          </List>
+          
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+         
+          <p className="dashboard-container col-bg">
+          Hello! {this.props.name}
+          </p>
+      
+        </main>
+      </div>
+    );
+  }
+}
+
+Dashboard.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+    
+    const mapStateToProps = state => {
+     const { currentUser } = state.auth;
+      return {
+        loggedIn: state.auth.currentUser !== null,
+        username: state.auth.currentUser.username,
+        name: `${currentUser.firstName} ${currentUser.lastName}`,
+        id: `${currentUser.id}`
+      };
+    
+    };
+    Dashboard = withStyles(styles, { withTheme: true })(Dashboard);
+    export default requiresLogin()(connect(mapStateToProps)(withRouter(Dashboard)));
+    
---
+++ src/store.js
@@ -0,0 +1,23 @@
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {reducer as formReducer} from 'redux-form';
+import thunk from 'redux-thunk';
+import {loadAuthToken} from './local-storage';
+import authReducer from './components/reducers/auth';
+import {setAuthToken, refreshAuthToken} from './components/actions/auth';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(
+    combineReducers({
+        form: formReducer,
+        auth: authReducer,
+    }),composeWithDevTools(
+    applyMiddleware(thunk)
+));
+const authToken = loadAuthToken();
+if(authToken){
+    const token = authToken;
+    store.dispatch(setAuthToken(token));
+    store.dispatch(refreshAuthToken());
+}
+
+export default store;
